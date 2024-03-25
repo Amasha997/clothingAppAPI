a11ee887b9b91e53bd3b7efac63f63d9e9618582
=Amasha Nuwanthi <=amashanuwanthi777@gmail.com>
2024-03-25 11:33:18 +0530

    initialComimt

---
+++ models/productModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        id: {
+            type: Number,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        category: {
+            type: String,
+            required: false,
+        },
+        imageURL: {
+            type: String,
+            required: false,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        size: {
+            type: String,
+            required: false,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        
+        colour: {
+            type: String,
+            required: false,
+        }
+        
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
